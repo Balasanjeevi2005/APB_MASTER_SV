16f6330f2e67a8f079b61aa8b89839653f35a72c
Balasanjeevi2005 <balasanjeevi0205@gmail.com>
2026-08-25 12:55:37 +0530

    Create test_pkg package with includes

---
+++ TB/test_pkg.sv
@@ -0,0 +1,14 @@
+package test_pkg;
+  
+  `include "defines.svh";
+  `include "transaction.sv";
+  `include "generator.sv";
+  `include "input_driver.sv";
+  `include "input_monitor.sv";
+  `include "output_monitor.sv";
+  `include "ref_model.sv";
+  `include "scoreboard.sv";
+  `include "env.sv"
+  `include "test.sv";
+
+endpackage
